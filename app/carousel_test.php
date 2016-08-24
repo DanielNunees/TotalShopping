588011a0547043dbd042186fd4a5ee450191f45e
Daniel <danielrns15@gmail.com>
2016-08-24 09:50:13 -0300

    Apagando

--- app/carousel_test.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="pt-BR" >
-<head>
-</head>
-<body>
-<h1>Alo mundo</h1>
-<input type="button" onclick="" value="Click me"></input>
-</body>
-</html>
--- banco/configuration.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	define('DB_HOSTNAME', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', null);
-	define('DB_DATABASE', 'bitnami_prestashop');
-	define('DB_PREFIX', 'ps');
-	define('DB_CHARSET', 'utf8');
